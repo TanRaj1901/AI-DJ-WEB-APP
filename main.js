98c5c4387fa0f2c936b7b9e48e4acae94a82e5fa
Tanmay Santosh Rajapure <75247451+TanRaj1901@users.noreply.github.com>
2022-02-05 13:31:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload() {
     song = loadSound("music.mp3");
@@ -10,6 +14,9 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
 }
 
 function draw() {
@@ -17,5 +24,24 @@ function draw() {
 }
 
 function play() {
-    son.play();
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded() {
+    console.log('Posenet Is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " | leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " | rightWristY = " + rightWristY);
+    }
 }
