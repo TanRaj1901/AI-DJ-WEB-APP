6ec3672b4194b81267b1a74673fd3ee89ae06837
Tanmay Santosh Rajapure <75247451+TanRaj1901@users.noreply.github.com>
2022-02-18 14:22:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,6 +21,17 @@ function setup() {
 
 function draw() {
     image(video, 0 , 0 , 600 , 500);
+
+    fill("#FF0000");
+    stroke('#FF0000');
+    if(scoreLeftWrist > 0.2) {
+        circle(leftWristX,leftWristY,20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_deciamls = floor(InNumberleftWristY);
+        volume = remove_decimals/500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 
 function play() {
@@ -36,6 +47,9 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " | leftWristY = " + leftWristY);
